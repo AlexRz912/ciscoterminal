b4380b063f9dc278b2e7d1bc5f00729e3ffa5b29
AlexRz912 <alexandre.rodriguez.alrz@gmail.com>
2025-10-06 17:00:31 +0200

    testing python terminal

---
+++ main.py
@@ -0,0 +1,33 @@
+
+from textual.app import App
+
+from textual.widgets import Button, Input, Static
+
+
+class ButtonsAndInputsApp(App):
+    CSS_PATH = "tcss"
+    def compose(self):
+        yield Static("(Config)#:")
+
+        
+if __name__ == "__main__":
+
+    app = ButtonsAndInputsApp()
+    app.run()
+
+
+
+
+
+# use compose for composing the backbone elements of the TUI
+        # Buttons
+        #yield Button("Click me!")
+        #yield Button("Primary!", variant="primary")
+        #yield Button.success("Success!")
+        #yield Button.warning("Warning!")
+        #yield Button.error("Error!")
+
+        # Inputs
+        #yield Input()
+        #yield Input(placeholder="Password", password=True)
+        #yield Input(placeholder="Type a number here",type="number",tooltip="Digits only please!",)
