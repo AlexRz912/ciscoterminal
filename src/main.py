dede6ad11f3c2c7cde0e2264ff0cfdeb1e199a44
AlexRz912 <alexandre.rodriguez.alrz@gmail.com>
2025-10-06 18:24:10 +0200

    [Buggy] implemented tree, has a weird bug with multiple child handling

--- src/main.py
+++ src/main.py
@@ -1,2 +1,18 @@
-if __name__ == "__main__":
-    
+from interface import InterfaceLevel
+from tree import TerminalTree, Node
+
+user_mode = Node(InterfaceLevel("user exec mode", ">:", "enable"))
+privileged_mode = Node(InterfaceLevel("privileged exec mode", "#:", "config_terminal"))
+user_mode.provide_child(privileged_mode)
+global_mode = Node(InterfaceLevel("global config mode", "(config)#:", "fa0/0"))
+privileged_mode.provide_child(global_mode)
+
+config_interfaces_mode = [
+    Node(InterfaceLevel("interface", "##:", "blob")),
+    Node(InterfaceLevel("router", "##:", "blob")),
+    Node(InterfaceLevel("line", "###:", "blob"))
+]
+global_mode.provide_child(config_interfaces_mode)
+
+interface_tree = TerminalTree(user_mode)
+interface_tree.prompt_tree()
--- src/tree.py
+++ src/tree.py
@@ -6,18 +6,26 @@ class TerminalTree():
         self.firstLevelNode.test_prompt()
 
 class Node():
-    def __init__(self, levelObject, childLevelObjects):
+    def __init__(self, levelObject):
         self.levelObject = levelObject
-        self.nextLevelObjects = childLevelObjects
+        self.nextLevelObjects = None
 
     def test_prompt(self):
+        if self.nextLevelObjects == None:
+            print(self.levelObject.name)
+            return
+        
         if self.__has_multiple_childs():
             for levelObject in self.nextLevelObjects:
-                print(levelObject.level)
+                print(levelObject.test_prompt(), levelObject.levelObject.name)
             return
-        print(self.levelObject.level)
+        
+        print(self.levelObject.name)
         self.nextLevelObjects.test_prompt()
 
+    def provide_child(self, child):
+        self.nextLevelObjects = child
+
     def __has_multiple_childs(self):
-        return isinstance(self.nextLevelObject.name, list)
+        return isinstance(self.nextLevelObjects, list)
     
